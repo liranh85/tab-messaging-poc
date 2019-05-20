98f59a3f843a2266d52949b6348c3197c8fe6a6f
Liran Harary <liran.harary@chargeafter.com>
2019-05-20 14:52:01 +0300

    broadcast-channel: starting from local-storage code

---
+++ broadcast-channel/tab2.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    init()
+})
+
+function init() {
+    console.log('In tab2')
+    message_broadcast({ command: 'doit', data: 'Hello from tab2!' })
+}
+
+function message_broadcast(message) {
+    localStorage.setItem('message', JSON.stringify(message))
+    localStorage.removeItem('message')
+}
