51159febefe78dadc4f3aca1a4b0ff7b44e23f06
Liran Harary <liran.harary@chargeafter.com>
2019-05-20 14:54:21 +0300

    Amend localStorage message

--- local-storage/tab2.js
+++ local-storage/tab2.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function init() {
     console.log('In tab2')
-    message_broadcast({ command: 'doit', data: 'Hello from tab2!' })
+    message_broadcast({ command: 'doit', data: 'Hello from tab2 using localStorage!' })
 }
 
 function message_broadcast(message) {
